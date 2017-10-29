0d7508c73d804f8bbd815387b71dc8153c4af53b
patricli <patricli@cisco.com>
2017-10-29 17:47:07 -0400

    added comments 10/29/2017

---
+++ euler_ex3.py
@@ -0,0 +1,29 @@
+var = 200
+
+
+def prime_factor(foo):
+    div = 2;
+    while foo % div:
+        div = div + 1
+    return div
+
+
+
+def main():
+    print("Hello world")
+    largest_prime = 1
+    if(var=1):  #var is 1, prime
+        print(largest_prime)
+    elif(var//largest_prime == 1):  #found largest prime
+        print(largest_prime)
+    else:
+        
+
+
+
+
+    print(prime_factor(var))
+
+
+if __name__ == "__main__":
+    main()
--- sample2.py
+++ sample2.py
@@ -1,13 +1,13 @@
 import random
 
 
-class Card():
+class Card():   #an object for cards with value suit and num
     def __init__(self, suit, num):
 
-        self.suit = suit
-        self.num = num
+        self.suit = suit  #int
+        self.num = num    #int
 
-    def show_card(self):
+    def show_card(self):    #returns card in human readable format
         suit_list = ["Diamond","Club","Heart","Spade"]
         if (self.num == 1):
             return "Ace of " + suit_list[self.suit]
@@ -21,7 +21,8 @@ class Card():
             return str(self.num) + " of " + suit_list[self.suit]
 
 class Deck:
-    def __init__(self):
+    #initialize deck as a list of 52 cards with suit from 0-3, and num from 1-13
+    def __init__(self): #initialize
         self.data = []
 
         #Suits = ["Spade", "Heart", "Club", "Diamond"]
@@ -32,20 +33,23 @@ class Deck:
                 #print(a.suit)
                 #print(a.num)
                 self.data.append(a)
-        self.len = len(self.data)
+        self.len = len(self.data)  #cards left in deck ??should we take outside init?
 
+    #delete remainder if self.len works
     def remainder(self):
         count = 0
         for i in self.data:
             count = count+1
         return count
 
+    #generate a random int and pop that index position from the deck
     def draw_card(self):
         index = random.randint(0,self.len-1)
         print("Random index = " + str(index))
         return self.data.pop(index)
 
 
+#validation tests
 a_Deck = Deck()
 print(a_Deck.remainder())
 print(a_Deck.len)
